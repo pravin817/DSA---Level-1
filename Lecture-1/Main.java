6417e3c79d8c7ff495956a7649c43c46100f7165
Pravin Mhaske <pravinmhaske817@gmail.com>
2025-11-11 23:02:44 +0530

    Lecture-1 : Basic Output

---
+++ Lecture-1/Main.java
@@ -0,0 +1,30 @@
+/**
+ * Problem Statement : Print the below triangle.
+ * For n = 5
+ *
+ * *
+ * **
+ * ***
+ * ****
+ * *****
+ */
+
+public class Main {
+    public static void main(String args[]) {
+
+        // 1. Print the "Hello World" to the console.
+        // System.out.println("Hello World");
+
+        // 2. Print the Triangle by using the multiple print statement
+
+        // System.out.println("*");
+        // System.out.println("**");
+        // System.out.println("***");
+        // System.out.println("****");
+        // System.out.println("*****");
+
+        // 3. Printing the Triangle by using the single print statement
+        System.out.print("*\n**\n***\n****\n*****");
+
+    }
+}
