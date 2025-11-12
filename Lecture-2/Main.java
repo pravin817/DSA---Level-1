2c588d21c7d6f78cd36f6bf2988e20d2804d4c78
Pravin Mhaske <pravinmhaske817@gmail.com>
2025-11-12 22:47:18 +0530

    Lecture-02 and 04 added.

---
+++ Lecture-2/Main.java
@@ -0,0 +1,23 @@
+/**
+ * Problem Statement : Print the below triangle.
+ * For n = 5
+ *
+ * *****
+ *    *
+ *   *
+ *  *
+ * *****
+ *
+ * Author : Pravin Mhaske
+ * Date   : 12/11/2025
+ */
+
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("*****");
+        System.out.println("   *");
+        System.out.println("  *");
+        System.out.println(" *");
+        System.out.println("*****");
+    }
+}
---
+++ Lecture-4/Main.java
@@ -0,0 +1,18 @@
+/**
+ * Task   : Learn the basics
+ * Author : Pravin Mhaske
+ * Date   : 12/11/2025
+ */
+public class Main {
+    public static void main(String args[]) {
+        int x = 15;
+        int y = 10;
+
+        System.out.println("The sum of " + x + " and " + y + " is " + (x + y));
+        System.out.println("The product of " + x + " and " + y + " is " + (x * y));
+        System.out.println("The division of " + x + " and " + y + " is " + (x / y));
+        System.out.println("The remainder of " + x + " and " + y + " is " + (x % y));
+        System.out.println("The value of xy / x + y is : " + (x * y) / (x + y));
+
+    }
+}
